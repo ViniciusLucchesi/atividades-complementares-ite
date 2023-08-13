54028b47f043af739c80b30f3404d21bfbc92bbf
Vinicius Lucchesi <viniciuslucchesi.ti@gmail.com>
2023-08-13 13:07:20 -0300

    Changing the file name from V2 routes

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from app.routes.v1 import activities
-from app.routes.v2 import pandas
+from app.routes.v2 import activities_v2
 
 
 app = FastAPI()
 
 app.include_router(activities.router)
-app.include_router(pandas.router)
+app.include_router(activities_v2.router)
