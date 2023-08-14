1636ff8802bbc2ca5b8a3ae5823bb283fdfd9763
Vinicius Lucchesi <viniciuslucchesi.ti@gmail.com>
2023-08-14 09:27:01 -0300

    Changing the file import name

--- app/main.py
+++ app/main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
-from app.routes.v1 import activities
-from app.routes.v2 import activities_v2
+from app.routes import activities
 
 
 app = FastAPI()
 
 app.include_router(activities.router)
-app.include_router(activities_v2.router)
---
+++ app/routes/activities.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from typing import Annotated
+from app.scraping import activities, auth_activities
+
+
+router = APIRouter(
+    prefix='/api/v1',
+    tags=['v1']
+)
+
+security = HTTPBasic()
+
+
+
+@router.get('/activities')
+async def get_activities(group: int = None):
+    if group is not None:
+        if group not in range(1, 5):
+            return [{'error': 'O parâmetro (group) deve ser de 1 á 4'}]
+    
+    data = activities.get_activities(group)
+    if len(data) == 0:
+        return [{'error': f'Não há atividades para o grupo {group}'}]
+    return data
+
+
+@router.get('/activities/auth')
+async def get_activity_link(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
+    payload = {
+        "matricula": credentials.dict()['username'],
+        "password": credentials.dict()['password']
+    }
+    return auth_activities.get_activities_link(payload)
