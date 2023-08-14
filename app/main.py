206db6cce17115f57b103a0982b8c316821a37b7
Vinicius Lucchesi <viniciuslucchesi.ti@gmail.com>
2023-08-14 09:32:18 -0300

    Set aditional information about the API

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from app.routes import activities
 
 
-app = FastAPI()
+app = FastAPI(
+    title="ITE Activities API",
+    version="2.0.5"
+)
 
 app.include_router(activities.router)
