7ca7f1fec85505d802ab76d8299f54a6807a9623
Vinicius Lucchesi <viniciuslucchesi.ti@gmail.com>
2023-09-20 15:43:54 -0300

    Connecting and closeing the connection from SurrealDB when the API start and stop

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from app.routes import activities
+from app.routes import subscriptions
+from app.database.surrealdb import SurrealDB
 
 
 app = FastAPI(
@@ -7,4 +9,16 @@ app = FastAPI(
     version="2.0.5"
 )
 
+
+@app.on_event('startup')
+async def startup():
+    await SurrealDB.connect()
+
+@app.on_event('shutdown')
+async def shutdown():
+    await SurrealDB.close()
+
+
+
 app.include_router(activities.router)
+app.include_router(subscriptions.router)
